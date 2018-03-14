ddde9e07016e62443b7fb35f751c159e7840a4c2
LessFilling1 <>
2018-03-14 14:29:35 +0000

    добавла 2 лаба

---
+++ revert_string.c
@@ -0,0 +1,28 @@
+#include "revert_string.h"
+
+
+void Swap(char *left, char *right)
+{
+	char copy = *left;
+	*left = *right;
+	*right = copy;
+}
+
+
+void RevertString(char *str)
+{
+    int i,j,len=0;
+    
+    
+        while(*(str + i + 1) != '\0'){i++;}
+        
+        len=i;
+        
+        for(i=0; i<len; i++){
+            for(j=0; j<len-i;j++){
+                Swap(str+j, str+j+1);
+            }
+        }
+}
+
+
